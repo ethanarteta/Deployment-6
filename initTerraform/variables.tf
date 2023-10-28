924b1a2e6308b754e3a610f395ac1af4828e33ae
Ethan Arteta <129544035+ethanarteta@users.noreply.github.com>
2023-10-28 14:06:43 -0500

    Update variables.tf

--- initTerraform/variables.tf
+++ initTerraform/variables.tf
@@ -1,3 +1,11 @@
-variable "aws_access_key" {}
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true 
+}
 
-variable "aws_secret_key" {}
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true 
+}
